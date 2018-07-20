79619eb0f7a19c82829b4e19fcc690c2907fea58
Ellen Skipper <Ellen.Skipper@softwire.com>
2018-07-20 11:36:16 +0100

    Add postcode, if valid prints lat lon, invalid prints error

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const request = require('request');
+const readlineSync = require('readline-sync');
+
+// Get LAT LON from POSTCODE
+let postcode = readlineSync.question('Enter a postcode: ');
+request(`https://api.postcodes.io/postcodes/${postcode}`, function(error, response, body) {
+    if (error) {
+        throw error;
+    }
+
+
+    const asJSON = JSON.parse(body);
+    
+    if (asJSON['status'] === 404) {
+        console.log('Invalid postcode');
+        return;
+    }
+
+    const latitude = asJSON['result']['latitude'];
+    const longitude = asJSON['result']['longitude'];
+    console.log(latitude, longitude);
+});
+
 
 const appId = '4586bfa0';
 const appKey = '4236304488967b5683897c8980725377';
 const stopId = '490008660N';
 const requestURL = `https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals?app_id=${appId}&app_key=${appKey}`;
+
 
 request(requestURL, function (error, response, body) {
     if (error) {
