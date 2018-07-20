071790735cc25edff4a69fcf77a9f1f3b0628773
Ellen Skipper <Ellen.Skipper@softwire.com>
2018-07-20 13:34:56 +0100

    Displays buses for a given stopid but postcode broken :(

--- index.js
+++ index.js
@@ -1,43 +1,62 @@
 const request = require('request');
 const readlineSync = require('readline-sync');
 
+// API Credentials
+const appId = '4586bfa0';
+const appKey = '4236304488967b5683897c8980725377';            
+
 // Get LAT LON from POSTCODE
+// Prompt user
 let postcode = readlineSync.question('Enter a postcode: ');
+
 request(`https://api.postcodes.io/postcodes/${postcode}`, function(error, response, body) {
     if (error) {
         throw error;
     }
 
+    const postcodeInfoAsJSON = JSON.parse(body);
 
-    const asJSON = JSON.parse(body);
-    
-    if (asJSON['status'] === 404) {
+    // Handle user entering garbage postcode
+    if (postcodeInfoAsJSON['status'] === 404) {
         console.log('Invalid postcode');
         return;
     }
 
-    const latitude = asJSON['result']['latitude'];
-    const longitude = asJSON['result']['longitude'];
-    console.log(latitude, longitude);
+    // Extract postcode lat lon
+    const latitude = postcodeInfoAsJSON['result']['latitude'];
+    const longitude = postcodeInfoAsJSON['result']['longitude'];
+    const radius = 1000; 
+    const busStopURL = `https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanOnstreetBusCoachStopPair&radius=${radius}&lat=${latitude}&lon=${longitude}`;
+
+    request(busStopURL, function(error, response, body) {
+        const stopPointsAsJSON = JSON.parse(body);
+
+        // Get first two closests bus stops
+        stopPointsAsJSON['stopPoints'].slice(0,10).forEach(function(stop) {
+            // const stopId = '490008660N';
+            const stopId = stop['naptanId'];
+            console.log(stopId);
+            const requestURL = `https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals?app_id=${appId}&app_key=${appKey}`;
+
+            // Get bus times from this stop
+            request(requestURL, function (error, response, body) {
+                if (error) {
+                    throw error;
+                }
+
+                const busTimeAsJSON = JSON.parse(body);
+                if (busTimeAsJSON.length === 0) {
+                    console.log(`No bus data found for stop id ${stopId}`);
+                    return;
+                } 
+
+                // Get first 5 buses and print their info
+                busTimeAsJSON.slice(0,6).forEach(function(bus) {
+                    console.log(`${bus['expectedArrival']} ${bus['destinationName']}`);
+                });
+            });
+        });
+    });
 });
 
-
-const appId = '4586bfa0';
-const appKey = '4236304488967b5683897c8980725377';
-const stopId = '490008660N';
-const requestURL = `https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals?app_id=${appId}&app_key=${appKey}`;
-
-
-request(requestURL, function (error, response, body) {
-    if (error) {
-        throw error;
-    }
-    const asJSON = JSON.parse(body);
-
-    console.log(`The next 5 buses leaving from ${asJSON[1]['stationName']} are:`)
-    const limit = 5;
-    for (let i=0; i<limit; i++) {
-        console.log(`${asJSON[i]['expectedArrival']} ${asJSON[i]['destinationName']}`);
-    }
-});
 
