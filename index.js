8ae10df00029c00ecc333da7dd700aaec5404edb
Ellen Skipper <Ellen.Skipper@softwire.com>
2018-07-20 15:54:55 +0100

    Use native promise request function to chain all requests witout nesting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const request = require('request-promise-native');
 const readlineSync = require('readline-sync');
 
 // API Credentials
@@ -9,63 +9,54 @@ const appKey = '4236304488967b5683897c8980725377';
 // Prompt user
 let postcode = readlineSync.question('\nEnter a postcode: ');
 
-// create promise for postcode
-let postCodePromise = new Promise(function(resolve, reject) {  
-    // Request the postcode
-    // If get lat lon... proceed with lat lon
-    request(`https://api.postcodes.io/postcodes/${postcode}`, function(error, response, body) {
-        if (error) {
-            throw error;
-        }
-    
-        const postcodeInfoAsJSON = JSON.parse(body);
-    
-        // Handle user entering garbage postcode
-        if (postcodeInfoAsJSON['status'] === 404) {
-            reject('Invalid postcode');
-        }
-    
-        // Extract postcode lat lon
-        const latitude = postcodeInfoAsJSON['result']['latitude'];
-        const longitude = postcodeInfoAsJSON['result']['longitude'];
-        resolve({latitude, longitude});
-    });
-});
+request(`https://api.postcodes.io/postcodes/${postcode}`).then(body => {
+    // Handle user entering garbage postcode
+    let postcodeInfoAsJSON = JSON.parse(body);
+    if (postcodeInfoAsJSON['status'] === 404) {
+        throw 'Invalid postcode';
+    }
 
-postCodePromise.then((val) => (getStopPoints(val.latitude, val.longitude)),
-                     (err) => console.log(err));
-
- function getStopPoints(latitude, longitude) {
+    // Extract postcode lat lon
+    const latitude = postcodeInfoAsJSON['result']['latitude'];
+    const longitude = postcodeInfoAsJSON['result']['longitude'];
+    
     const radius = 1000; 
     const busStopURL = `https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanOnstreetBusCoachStopPair&radius=${radius}&lat=${latitude}&lon=${longitude}`;   
+    return request(busStopURL);
+    })
 
-    request(busStopURL, function(error, response, body) {
-        const stopPointsAsJSON = JSON.parse(body);
+.then(body => {
+    const stopPointsAsJSON = JSON.parse(body);
+    let promises = [];
 
-        // Get first two closests bus stops
-        stopPointsAsJSON['stopPoints'].slice(0,2).forEach(function(stop) {
-            stop['lineGroup'].forEach(function(directedStop) {
-                const stopId = directedStop['naptanIdReference'];
-                const requestURL = `https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals?app_id=${appId}&app_key=${appKey}`;
+    // Get first two closests bus stops
+    stopPointsAsJSON['stopPoints'].slice(0,2).forEach(function(stop) {
+        stop['lineGroup'].forEach(function(directedStop) {
+            const stopId = directedStop['naptanIdReference'];
+            const requestURL = `https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals?app_id=${appId}&app_key=${appKey}`;
 
-                // Get bus times from this stop
-                request(requestURL, function (error, response, body) {
-                    if (error) {
-                        throw error;
-                    }
-
-                    const busTimeAsJSON = JSON.parse(body);
-                    if (busTimeAsJSON.length === 0) {
-                        console.log(`No bus data found for stop id ${stopId}`);
-                        return;
-                    } 
-                    console.log(`\nThe next 5 buses departing from ${stop['commonName']} are:`);
-                    // Get first 5 buses and print their info
-                    busTimeAsJSON.slice(0,5).forEach(function(bus) {
-                        console.log(`${bus['expectedArrival']} ${bus['destinationName']}`);
-                    });
-                });
-            });
+            // Get bus times from this stop
+            promises.push(request(requestURL));
         });
+    });
+    return Promise.all(promises);
+})
+
+.then(bodies => {
+    bodies.forEach(body => printBusTimes(body));
+})
+
+.catch((err) => console.log("error:", err.message));
+
+function printBusTimes(body) {
+    const busTimeAsJSON = JSON.parse(body);
+    if (busTimeAsJSON.length === 0) {
+        console.log(`No bus data found`);
+        return;
+    } 
+    console.log(`\nThe next 5 buses departing are:`);
+    // Get first 5 buses and print their info
+    busTimeAsJSON.slice(0,5).forEach(function(bus) {
+        console.log(`${bus['expectedArrival']} ${bus['destinationName']}`);
     });
 }
