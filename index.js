2c19a0c0f2d0fbae491cc56579646bd924a7f522
Ellen Skipper <Ellen.Skipper@softwire.com>
2018-07-20 13:46:58 +0100

    Uses directed stops and actually gets data for nearby stations

--- index.js
+++ index.js
@@ -32,27 +32,28 @@ request(`https://api.postcodes.io/postcodes/${postcode}`, function(error, respon
         const stopPointsAsJSON = JSON.parse(body);
 
         // Get first two closests bus stops
-        stopPointsAsJSON['stopPoints'].slice(0,10).forEach(function(stop) {
-            // const stopId = '490008660N';
-            const stopId = stop['naptanId'];
-            console.log(stopId);
-            const requestURL = `https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals?app_id=${appId}&app_key=${appKey}`;
+        stopPointsAsJSON['stopPoints'].slice(0,2).forEach(function(stop) {
+            stop['lineGroup'].forEach(function(directedStop) {
+                const stopId = directedStop['naptanIdReference'];
+                console.log(stopId);
+                const requestURL = `https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals?app_id=${appId}&app_key=${appKey}`;
 
-            // Get bus times from this stop
-            request(requestURL, function (error, response, body) {
-                if (error) {
-                    throw error;
-                }
+                // Get bus times from this stop
+                request(requestURL, function (error, response, body) {
+                    if (error) {
+                        throw error;
+                    }
 
-                const busTimeAsJSON = JSON.parse(body);
-                if (busTimeAsJSON.length === 0) {
-                    console.log(`No bus data found for stop id ${stopId}`);
-                    return;
-                } 
+                    const busTimeAsJSON = JSON.parse(body);
+                    if (busTimeAsJSON.length === 0) {
+                        console.log(`No bus data found for stop id ${stopId}`);
+                        return;
+                    } 
 
-                // Get first 5 buses and print their info
-                busTimeAsJSON.slice(0,6).forEach(function(bus) {
-                    console.log(`${bus['expectedArrival']} ${bus['destinationName']}`);
+                    // Get first 5 buses and print their info
+                    busTimeAsJSON.slice(0,5).forEach(function(bus) {
+                        console.log(`${bus['expectedArrival']} ${bus['destinationName']}`);
+                    });
                 });
             });
         });
